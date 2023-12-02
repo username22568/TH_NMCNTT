ede5630091cb9354f3cafafa553f32cd0a18e7ba
username22568 <146604533+username22568@users.noreply.github.com>
2023-12-02 19:42:37 +0700

    Update maingame.py

--- MainGame/maingame.py
+++ MainGame/maingame.py
@@ -1,4 +1,5 @@
 import pygame, sys, time, random
+import tkinter as tk
 from pygame.locals import *
 pygame.init()
 
@@ -7,18 +8,18 @@ fpsClock = pygame.time.Clock()
 
 #Cửa sổ game
 screen_size=(1280,720)
-screen=pygame.display.set_mode(screen_size)
-pygame.display.set_caption("Car Bet")
-icon=pygame.image.load(r'MainGame\Image\car2.png')
-pygame.display.set_icon(icon)
+
 #Thêm background
 bg=pygame.transform.scale(pygame.image.load(r'MainGame\Image\background.png'),screen_size)
 end_bg=1100
 start_bg=200
+
 #Load ảnh xe
 car_pic2=pygame.transform.scale(pygame.image.load(r'MainGame\Image\car2.png'),(150,150))
-car_pic1=pygame.transform.scale(pygame.image.load(r'MainGame\Image\car1.png'),(150,150))
+car_pic1=pygame.transform.scale(pygame.image.load(r'MainGame\Image\car2.png'),(150,150))
 item_pic=pygame.image.load(r'MainGame\Image\item.png')
+#Thêm âm thanh
+
 #Màu
 white=(255,255,255)
 black=(0,0,0)
@@ -27,7 +28,7 @@ red=(255,0,0)
 
 #Set font
 font = pygame.font.SysFont("Arial", 50, bold=True, italic=False)
-
+new_font = pygame.font.SysFont("Arial", 30, bold=True, italic=False)
 #Hàm
 #Vẽ chữ
 def draw_text(text, font, color, surface, x, y):
@@ -37,15 +38,16 @@ def draw_text(text, font, color, surface, x, y):
     surface.blit(text_obj, text_rect)        
 #Class
 class Car():
-    def __init__(self,image,lane,buff_speed=False,better_start=False):
+    def __init__(self,screen,image,lane,buff_speed=False,better_start=False):
         self.image=image
         self.y=100+(lane-1)*140
         self.buff_speed=buff_speed
         self.better_start=better_start
+        self.screen=screen
     def draw(self,x,rank):
         self.x=x
         self.rect=self.image.get_rect(center=(self.x,self.y))
-        screen.blit(self.image,self.rect)
+        self.screen.blit(self.image,self.rect)
         self.rank=rank
     def is_in(self,x,y):
         if self.x==x and self.y==y:
@@ -63,9 +65,10 @@ class Item():
     exist=False
     def __init__(self):
         pass
-    def draw(self,x,y):
+    def draw(self,x,y,screen):
+        self.screen=screen
         self.rect=self.image.get_rect(center=(x,y))
-        screen.blit(self.image,self.rect)
+        self.screen.blit(self.image,self.rect)
     def affect(self,x):
         self.type=random.randint(0,5)
         if self.type==0 or self.type==2:
@@ -76,27 +79,31 @@ class Item():
             x=end_bg
         return x
 class Buttons():
-    def __init__(self,height,width,image,x,y):
+    def __init__(self,height,width,image,x,y,screen):
         self.height=height
         self.width=width
         self.img=pygame.transform.scale(image,(height,width))
         self.x=x
         self.y=y
         self.rect=self.img.get_rect()
+        self.screen=screen
     def draw(self):
         self.rect.topleft=(self.x,self.y)
-        screen.blit(self.img,self.rect)
+        self.screen.blit(self.img,self.rect)
     def is_in(self,x,y):
         if x>self.x and x<self.x+self.height and y>self.y and y<self.y+self.width:
             return True
+
+
+
 #Vào đua
-def run_game(player_pic,com1_pic,com2_pic,com3_pic,com4_pic,buff_speed,better_start):
+def run_game(player_pic,com1_pic,com2_pic,com3_pic,com4_pic,buff_speed,better_start,screen):
     #Khởi tạo xe 
-    player=Car(player_pic,2,buff_speed,better_start)
-    com1=Car(com1_pic,1)
-    com2=Car(com2_pic,3)
-    com3=Car(com3_pic,4)
-    com4=Car(com4_pic,5)
+    player=Car(screen,player_pic,2,buff_speed,better_start)
+    com1=Car(screen,com1_pic,1)
+    com2=Car(screen,com2_pic,3)
+    com3=Car(screen,com3_pic,4)
+    com4=Car(screen,com4_pic,5)
     #Khởi tạo biến item
     item1=Item()
     item2=Item()
@@ -124,6 +131,7 @@ def run_game(player_pic,com1_pic,com2_pic,com3_pic,com4_pic,buff_speed,better_st
     bg_x=0
     rank=1
     ranked=False
+    check_rank = True
     #Vòng lặp game
     start=False
     while running:
@@ -164,25 +172,25 @@ def run_game(player_pic,com1_pic,com2_pic,com3_pic,com4_pic,buff_speed,better_st
                         num_item+=1
                 #Vẽ item 1
                 if item1.exist:
-                    item1.draw(item1_x,item1_y)
+                    item1.draw(item1_x,item1_y,screen)
                     #Xử lý nếu xe chạm vào item
-                    if player.is_in(item1_x,item1_y):
+                    if player.is_in(item1_x-50,item1_y):
                         player_x=item1.affect(player_x)
                         num_item-=1
                         item1.exist=False
-                    if com1.is_in(item1_x,item1_y):
+                    if com1.is_in(item1_x-50,item1_y):
                         com1_x=item1.affect(com1_x)
                         num_item-=1
                         item1.exist=False
-                    if com2.is_in(item1_x,item1_y):
+                    if com2.is_in(item1_x-50,item1_y):
                         com2_x=item1.affect(com2_x)
                         num_item-=1
                         item1.exist=False
-                    if com3.is_in(item1_x,item1_y):
+                    if com3.is_in(item1_x-50,item1_y):
                         com3_x=item1.affect(com3_x)
                         num_item-=1
                         item1.exist=False
-                    if com4.is_in(item1_x,item1_y):
+                    if com4.is_in(item1_x-50,item1_y):
                         com4_x=item1.affect(com4_x)
                         num_item-=1
                         item1.exist=False
@@ -195,24 +203,24 @@ def run_game(player_pic,com1_pic,com2_pic,com3_pic,com4_pic,buff_speed,better_st
                         item2.exist=True
                         num_item+=1
                 if item2.exist:
-                    item2.draw(item2_x,item2_y)
-                    if player.is_in(item2_x,item2_y):
+                    item2.draw(item2_x,item2_y,screen)
+                    if player.is_in(item2_x-50,item2_y):
                         player_x=item2.affect(player_x)
                         num_item-=1
                         item2.exist=False
-                    if com1.is_in(item2_x,item2_y):
+                    if com1.is_in(item2_x-50,item2_y):
                         com1_x=item2.affect(com1_x)
                         num_item-=1
                         item2.exist=False
-                    if com2.is_in(item2_x,item2_y):
+                    if com2.is_in(item2_x-50,item2_y):
                         com2_x=item2.affect(com2_x)
                         num_item-=1
                         item2.exist=False
-                    if com3.is_in(item2_x,item2_y):
+                    if com3.is_in(item2_x-50,item2_y):
                         com3_x=item2.affect(com3_x)
                         num_item-=1
                         item2.exist=False
-                    if com4.is_in(item2_x,item2_y):
+                    if com4.is_in(item2_x-50,item2_y):
                         com4_x=item2.affect(com4_x)
                         num_item-=1
                         item2.exist=False
@@ -248,14 +256,19 @@ def run_game(player_pic,com1_pic,com2_pic,com3_pic,com4_pic,buff_speed,better_st
             draw_text(str(com2.rank),font,white,screen,end_bg+100,com2.y)
             draw_text(str(com3.rank),font,white,screen,end_bg+100,com3.y)
             draw_text(str(com4.rank),font,white,screen,end_bg+100,com4.y)
+            # bảng xếp hạng
+            check_rank = ranked_rs(r'MainGame\Image\item.png',screen_size[0]/2,4*screen_size[1]/5,player,com1,com2,com3,com4,screen)
 
         pygame.display.update()
+        #thoát ra menu
+        if check_rank ==False:
+            break
     return player_rank
-def shopping():
+def shopping(screen):
     #Khởi tạo nút
-    buy_buff_speed=Buttons(200,100,car_pic1,340,360)
-    buy_better_start=Buttons(200,100,car_pic1,740,360)
-    quit_bt=Buttons(200,100,item_pic,540,570)
+    buy_buff_speed=Buttons(200,100,car_pic1,340,360,screen)
+    buy_better_start=Buttons(200,100,car_pic1,740,360,screen)
+    quit_bt=Buttons(200,100,item_pic,540,570,screen)
     #Tham số
     buff_speed=False
     better_start=False
@@ -282,10 +295,18 @@ def shopping():
         pygame.display.update()
     return (buff_speed,better_start)
 #Sảnh chờ
+
+
+
+
 def main_menu():
+    screen=pygame.display.set_mode(screen_size)
+    pygame.display.set_caption("Car Bet")
+    icon=pygame.image.load(r'MainGame\Image\item.png')
+    pygame.display.set_icon(icon)
     #Khởi tạo nút
-    start_bt=Buttons(200,100,item_pic,540,570)
-    shop_bt=Buttons(100,100,item_pic,50,570)
+    start_bt=Buttons(200,100,item_pic,540,570,screen)
+    shop_bt=Buttons(100,100,item_pic,50,570,screen)
     buff=(False,False)
     #Vòng lặp
     running_menu=True
@@ -298,15 +319,112 @@ def main_menu():
                 #Xử lý thao tác trên các nút
                 if start_bt.is_in(spot[0],spot[1]):
                     running_menu=False
-                    run_game(car_pic2,car_pic2,car_pic2,car_pic2,car_pic2,buff[0],buff[1])
+                    run_game(car_pic2,car_pic2,car_pic2,car_pic2,car_pic2,buff[0],buff[1],screen)
                     running_menu=True
                 if shop_bt.is_in(spot[0],spot[1]):
                     running_menu=False
-                    buff=shopping()
+                    buff=shopping(screen)
                     running_menu=True
         screen.fill(black)
         #Vẽ nút
         shop_bt.draw()
         start_bt.draw()
         pygame.display.update()
-main_menu()
+
+def ranked_rs(image,width,height,player,com1,com2,com3,com4,screen):
+    rank_img = pygame.transform.scale(pygame.image.load(image),(width,height))
+    rank_rect = rank_img.get_rect(topleft=(screen_size[0]/4,screen_size[1]/10))
+    running_rank = True
+    home_bt = Buttons(screen_size[0]/16,screen_size[1]/8,item_pic,screen_size[0]/4,9*screen_size[1]/10,screen)
+    lt = [player,com1,com2,com3,com4]
+    # sắp xếp thứ tự xếp hạng
+    j=0
+    while j < len(lt):
+        i=j
+        sw=False
+        while i < len(lt)-1:
+            if lt[i].rank > lt[i+1].rank :
+                sw = True
+                temp=lt[i]
+                lt[i]=lt[i+1]
+                lt[i+1]=temp
+            i+=1
+        if sw == False:
+            break
+        
+
+
+    #tên tạm thời
+    player.name ="player"
+    com1.name ="com1"
+    com2.name ="com2"
+    com3.name ="com3"
+    com4.name ="com4"
+
+
+    while running_rank:
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                pygame.quit()
+            if event.type==pygame.MOUSEBUTTONDOWN:
+                spot=event.pos
+                if home_bt.is_in(spot[0],spot[1]):
+                    running_rank = False
+                    main_menu()
+
+        screen.blit(rank_img,rank_rect)
+        #vẽ nút
+        home_bt.draw()
+        draw_text('Home',new_font,red,screen,home_bt.x+(screen_size[0]/16)/2,home_bt.y+(screen_size[1]/8)/2)
+        draw_text('1',new_font,black,screen,screen_size[0]/4+screen_size[0]/40,3*screen_size[1]/20)
+        draw_text('2',new_font,black,screen,screen_size[0]/4+screen_size[0]/40,6*screen_size[1]/20)
+        draw_text('3',new_font,black,screen,screen_size[0]/4+screen_size[0]/40,9*screen_size[1]/20)
+        draw_text('4',new_font,black,screen,screen_size[0]/4+screen_size[0]/40,12*screen_size[1]/20)
+        draw_text('5',new_font,black,screen,screen_size[0]/4+screen_size[0]/40,15*screen_size[1]/20)
+        draw_text(str(lt[0].name),new_font,black,screen,screen_size[0]/4+screen_size[0]/5,3*screen_size[1]/20)
+        draw_text(str(lt[1].name),new_font,black,screen,screen_size[0]/4+screen_size[0]/5,6*screen_size[1]/20)
+        draw_text(str(lt[2].name),new_font,black,screen,screen_size[0]/4+screen_size[0]/5,9*screen_size[1]/20)
+        draw_text(str(lt[3].name),new_font,black,screen,screen_size[0]/4+screen_size[0]/5,12*screen_size[1]/20)
+        draw_text(str(lt[4].name),new_font,black,screen,screen_size[0]/4+screen_size[0]/5,15*screen_size[1]/20)
+        pygame.display.flip()
+    return False
+
+def login():
+    username = entry_username.get()
+    password = entry_password.get()
+    
+    # Kiểm tra thông tin đăng nhập
+    if username == "admin" and password == "password":
+        # Đăng nhập thành công, chuyển màn hình
+        window_login.destroy()  # Đóng màn hình đăng nhập
+        
+        # Tạo màn hình mới sau khi đăng nhập thành công
+        main_menu()
+        
+        # Thêm các thành phần và chức năng cho màn hình mới ở đây
+        
+    else:
+        # Đăng nhập không thành công, hiển thị thông báo lỗi
+        label_error.config(text="Thông tin đăng nhập không đúng")
+
+# Tạo màn hình đăng nhập
+window_login = tk.Tk()
+
+# Thêm các thành phần vào màn hình đăng nhập
+label_username = tk.Label(window_login, text="Tên đăng nhập:")
+label_username.pack()
+entry_username = tk.Entry(window_login)
+entry_username.pack()
+
+label_password = tk.Label(window_login, text="Mật khẩu:")
+label_password.pack()
+entry_password = tk.Entry(window_login, show="*")
+entry_password.pack()
+
+button_login = tk.Button(window_login, text="Đăng nhập", command=login)
+button_login.pack()
+
+label_error = tk.Label(window_login, text="")
+label_error.pack()
+
+# Chạy màn hình đăng nhập
